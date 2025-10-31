d17df60169d3efd68ba921176792804c721537c9
Lautaro-Figueroa <lautarofigueroalnf@gmail.com>
2025-10-30 23:18:04 -0300

    feat(sql): agrego script con tablas Articulos, ImagenesArticulo y Auditoria

--- Creacion de DB/TPI_Grupo50_SQLQuery1.sql
+++ Creacion de DB/TPI_Grupo50_SQLQuery1.sql
@@ -1,16 +1,31 @@
-CREATE DATABASE BD2_TPI_GRUPO_50
-COLLATE Latin1_General_CI_AI
-GO
-
 USE BD2_TPI_GRUPO_50
 GO
 
-CREATE TABLE Categorias (
-    IdCategoria INT PRIMARY KEY IDENTITY(1,1),
-    NombreCategoria VARCHAR(50) NOT NULL
+CREATE TABLE Articulos (
+    IdArticulo INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+    NombreArticulo VARCHAR(50) NOT NULL,
+    Descripcion VARCHAR(100) NULL,
+    Precio DECIMAL(10,2) NOT NULL CHECK (Precio > 0),
+    IdCategoria INT NOT NULL,
+    IdEstado INT NOT NULL,
+    FOREIGN KEY (IdCategoria) REFERENCES Categorias(IdCategoria),
+    FOREIGN KEY (IdEstado) REFERENCES EstadosArticulo(IdEstado)
 );
+GO
 
-CREATE TABLE EstadosArticulo (
-    IdEstado INT PRIMARY KEY IDENTITY(1,1),
-    NombreEstado VARCHAR(50) NOT NULL
+CREATE TABLE ImagenesArticulo (
+    IdImagen INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+    IdArticulo INT NOT NULL,
+    RutaImagen VARCHAR(350) NULL,
+    FOREIGN KEY (IdArticulo) REFERENCES Articulos(IdArticulo)
 );
+GO
+
+CREATE TABLE Auditoria (
+    IdAuditoria INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+    IdArticulo INT NOT NULL,
+    Fecha DATETIME NOT NULL DEFAULT(GETDATE()),
+    Mensaje VARCHAR(255) NOT NULL,
+    FOREIGN KEY (IdArticulo) REFERENCES Articulos(IdArticulo)
+);
+GO
